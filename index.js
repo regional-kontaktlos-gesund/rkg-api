eea4b736253911b15db5301305de9de03bfcbc8a
donmatheo <dev@donmatheo.de>
2020-03-21 15:43:27 +0100

    add vendor routes

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
 
 // routes for vendors
 const vendorsRouter = require('./routes/vendors')
-app.use('/vendors', vendorsRouter)
+app.use('/api/vendors', vendorsRouter)
 
 
 // run app
--- routes/vendors.js
+++ routes/vendors.js
@@ -5,47 +5,72 @@ const Vendor = require('../models/vendor')
 
 // get all vendors
 router.get('/', async (req, res) => {
-    try {
-        const vendors = await Vendor.find()
-        res.json(vendors)
-    } catch (err) {
-        res.status(500).json({ message: err.message })
-    }
+  try {
+    const vendors = await Vendor.find()
+    res.json(vendors)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
 })
 
-// Getting one vendor
+// getting one vendor
 router.get('/:id', getVendor, (req, res) => {
   res.json(res.vendor)
 })
 
 // create one vendor
-
 router.post('/', async (req, res) => {
-    const vendor = new Vendor({
-      name: req.body.name,
-      email: req.body.email
-    })
-  
-    try {
-      const newVendor = await vendor.save()
-      res.status(201).json(newVendor)
-    } catch (err) {
-      res.status(400).json({ message: err.message })
-    }
+  const vendor = new Vendor({
+    name: req.body.name,
+    email: req.body.email
   })
 
+  try {
+    const newVendor = await vendor.save()
+    res.status(201).json(newVendor)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
 
-  // Middleware function for gettig vendor object by ID
+// Updating one vendor
+router.patch('/:id', getVendor, async (req, res) => {
+  if (req.body.name != null) {
+    res.vendor.name = req.body.name
+  }
+
+  if (req.body.email != null) {
+    res.vendor.email = req.body.email
+  }
+  try {
+    const updatedVendor = await res.vendor.save()
+    res.json(updatedVendor)
+  } catch {
+    res.status(400).json({ message: err.message })
+  }
+})
+
+// delete one vendor
+router.delete('/:id', getVendor, async (req, res) => {
+  try {
+    await res.vendor.remove()
+    res.json({ message: 'Deleted This Vendor' })
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+// Middleware function for gettig vendor object by ID
 async function getVendor(req, res, next) {
   try {
     vendor = await Vendor.findById(req.params.id)
     if (vendor == null) {
-      return res.status(404).json({ message: 'Cant find vendor'})
+      return res.status(404).json({ message: 'Cant find vendor' })
     }
-  } catch(err){
+  } catch (err) {
     return res.status(500).json({ message: err.message })
   }
-  
+
   res.vendor = vendor
   next()
 }

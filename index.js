8de742a63dd445ba938b3d10d8c3763d1213f406
donmatheo <dev@donmatheo.de>
2020-03-21 10:55:11 +0100

    set port for heroku

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });

8d257617a15d9ca23ab495d07836d4fa7b48f9b7
donmatheo <dev@donmatheo.de>
2020-03-22 17:47:27 +0100

    add swagger doc to project

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ var app = express();
 const checkJwt = require('./middlewares/auth0')
 const checkUser = require('./middlewares/checkUser')
 
+//swagger stuff
+var swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+//app.use('/api/', router);
+
 // register middlewares
 app.use(express.json())
 app.use(cors())
